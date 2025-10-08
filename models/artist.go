511a4b395c2eaf693d96dfa0babba0d7cd52cd6b
osamikoyo <v.pylayev@mail.ru>
2025-10-08 07:21:52 +0500

    update artist model

--- models/artist.go
+++ models/artist.go
@@ -10,19 +10,20 @@ import (
 const ArtistImageTemplate = "%s.png"
 
 type Artist struct {
-    gorm.Model
-    Name   string
+	gorm.Model
+	Name      string
 	ImagePath string
-    Albums []Album `gorm:"foreignKey:ArtistID"`
+	Desc      string
+	Albums    []Album `gorm:"foreignKey:ArtistID"`
 }
 
-
-func NewArtist(name string) *Artist {
+func NewArtist(name string, desc string) *Artist {
 	return &Artist{
 		Name: name,
+		Desc: desc,
 	}
 }
- 
+
 func (a *Artist) GetImageFilepath(imageDir string) string {
 	return filepath.Join(imageDir, fmt.Sprintf(ArtistImageTemplate, a.Name))
 }
